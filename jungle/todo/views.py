bf300e3d7e7db9c96d9512383b572af231e28727
johcodes19 <johcodes19@gmail.com>
2023-09-19 16:32:10 +0300

    added form verification ,redirecting users to a certain page, help the site remember you

--- jungle/todo/views.py
+++ jungle/todo/views.py
@@ -1,13 +1,41 @@
+
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
+from django import forms
+
 
 # Create your views here.
-tasks = ['wash utensils', 'do laundry', 'clean the garage']
+# a class to represent the form
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+
+
 def index(request):
-    return render(request, 'todo/index.html',{
-        'tasks':tasks,
-        
-    }
-                )
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    tasks = request.session["tasks"]
+    return render(request, 'todo/index.html', {
+        "tasks": tasks,
+    })
+
 
 def add(request):
-    return render(request, 'todo/add.html')
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks = request.session.get("tasks", [])
+            tasks.append(task)
+            request.session["tasks"] = tasks
+            return HttpResponseRedirect(reverse("todo:index"))
+        else:
+            return render(request, 'todo/add.html', {
+                "form": form
+            })
+    return render(request, 'todo/add.html', {
+        # a variable for the above form
+        "form": NewTaskForm()
+    })
